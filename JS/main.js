408671fb7b53c2d6aff02348ee4a048d66e960dc
Habibasobhy <habibasobhy288@gmail.com>
2024-12-16 01:00:50 +0200

    First-commit

---
+++ JS/main.js
@@ -0,0 +1,60 @@
+// & Exercise 1: Filtering an Array
+// & Problem: Write a function that takes an array of numbers
+// & and returns a new array containing only the even numbers.
+
+// function filterEvenNumbers(arr){
+//     return arr.filter(number => number % 2 === 0);
+// }
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8 , 9 , 10];
+// let evenNumbers = filterEvenNumbers(numbers);
+// console.log(evenNumbers);
+
+// ^ ==========================================================
+
+// & Exercise: 2 Find the Largest Number
+// & Problem: Write a function that takes an array of numbers
+// & and returns the largest number in the array.
+
+// function findLargestNumber(arr){
+//     return Math.max(...arr);
+// }
+
+// let numbers = [10, 20, 5, 6, 70, 8 , 90];
+// let maxNumber = findLargestNumber(numbers);
+// console.log(maxNumber);
+
+// ^ ==========================================================
+
+// & Exercise: 3 Reverse a String
+// & Problem: Write a function that takes a string as input and
+// & returns the same string but reversed.
+
+// function reverseString(str){
+//     return str.split('').reverse().join('');
+// }
+
+// let inpString = "Java Script";
+// let reservedString = reverseString(inpString);
+// console.log(reservedString);
+
+// ^ ==========================================================
+
+// & Exercise: 4 Remove Duplicates
+// & Problem: Write a function that takes an array of numbers
+// & and returns a new array without duplicate numbers.
+
+// function removeDuplicates(arr){
+//     let uniqueNumbers = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if(!uniqueNumbers.includes(arr[i]))
+//             uniqueNumbers.push(arr[i]);
+//     }
+//     return uniqueNumbers;
+// }
+
+// let numbers = [1 , 3 , 5 , 10 , 3 , 5 , 6 , 8 , 10 , 9 , 1];
+// let uniqueNumbers = removeDuplicates(numbers);
+// console.log(uniqueNumbers);
+
+// ^ ==========================================================
